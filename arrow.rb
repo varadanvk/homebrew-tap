d3be9ba7ec8d7e2ec15feb8a42ae1cea38e39a87
varadanvk <126513116+varadanvk@users.noreply.github.com>
2025-04-15 18:32:20 -0700

    Update Arrow formula URL to version 0.0.1

--- arrow.rb
+++ arrow.rb
@@ -1,7 +1,7 @@
 class Arrow < Formula
     desc "Vector database with persistent storage built in Rust"
     homepage "https://github.com/varadanvk/arrow"
-    url "https://github.com/varadanvk/arrow/archive/refs/tags/v0.1.0.tar.gz"
+    url "https://github.com/varadanvk/arrow/archive/refs/tags/v0.0.1.tar.gz"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     license "MIT"
   
