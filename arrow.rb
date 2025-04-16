754995ff543e5b28187d00f0e2b11257e8c5adb8
varadanvk <126513116+varadanvk@users.noreply.github.com>
2025-04-16 10:04:28 -0700

    Update Arrow formula to version 0.0.2 with new SHA256 checksum

--- arrow.rb
+++ arrow.rb
@@ -1,8 +1,8 @@
 class Arrow < Formula
   desc "Vector database with persistent storage built in Rust"
   homepage "https://github.com/varadanvk/arrow"
-  url "https://github.com/varadanvk/arrow/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "860da5d51c99cfada48e902bfa493dda40f2201dcce50cfd4954931a5569165f"
+  url "https://github.com/varadanvk/arrow/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "7a100495e92d575afb1954b01899c7252a9719f5323e7e7d945b1ef4a831a8d9"
   license "MIT"
 
   depends_on "rust" => :build
